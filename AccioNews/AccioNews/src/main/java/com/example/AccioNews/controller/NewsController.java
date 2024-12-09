261de1dfbee77223f30ab2eb6c449e041b327829
Kunal Jindal <kunaljindal@microsoft.com>
2024-12-09 22:11:40 +0530

    Add news app

---
+++ AccioNews/AccioNews/src/main/java/com/example/AccioNews/controller/NewsController.java
@@ -0,0 +1,21 @@
+package com.example.AccioNews.controller;
+
+import com.example.AccioNews.service.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/news")
+public class NewsController {
+
+    @Autowired
+    NewsService newsService;
+
+    @GetMapping
+    public Object getNews(@RequestParam("api-key") String apiKey) {
+        return newsService.getNews(apiKey);
+    }
+}
